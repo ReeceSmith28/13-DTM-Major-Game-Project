36bc314f5bf0433a53bb741766a76db363740aa4
ReeceSmith28 <125227555+ReeceSmith28@users.noreply.github.com>
2023-07-24 15:03:16 +1200

    Added an enemy
    Added an enemy and attempted to get the chase and detection range to work

---
+++ Assets/Scripts/Enemy/AwareOfPlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwareOfPlayerController : MonoBehaviour
+{
+
+    public bool Awareofplayer { get; private set; }
+
+    public Vector2 Directiontoplayer { get; private set; }
+
+    [SerializeField]
+
+    private float Playerawarenessdistnce;
+
+    private Transform player;
+        private void Awake()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemytoPlayerVector = player.position - transform.position;
+        Directiontoplayer = enemytoPlayerVector.normalized;
+
+            if (enemytoPlayerVector.magnitude <= Playerawarenessdistnce)
+            {
+            Awareofplayer = true;        
+
+            }
+            else
+            {
+            Awareofplayer = false;
+            }
+        
+     }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float speed;
+
+    private AwareOfPlayerController playerAwarenessController;
+    private Vector2 targetDirection;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<AwareOfPlayerController>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (playerAwarenessController.Awareofplayer)
+        {
+            targetDirection = playerAwarenessController.Directiontoplayer;
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void SetVelocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+        else
+        {
+            rigidbody2D.velocity = transform.up * speed;
+        }
+    }
+}
