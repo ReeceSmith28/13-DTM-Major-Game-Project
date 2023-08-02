800d26f8224a63dec06949503b81eac4a7b0388f
ReeceSmith28 <125227555+ReeceSmith28@users.noreply.github.com>
2023-08-02 19:52:21 +1200

    Update HealthController.cs

--- Assets/Scripts/Health/HealthController.cs
+++ Assets/Scripts/Health/HealthController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthController : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class HealthController : MonoBehaviour
     [SerializeField]
     private float maximumHealth;
 
+    public bool isInvincible { get; set; }
+
     public float RemainingHealthPercentage
     {
         get
@@ -17,7 +20,9 @@ public class HealthController : MonoBehaviour
             return currentHealth / maximumHealth;
         }
     }
-    
+
+    public UnityEvent OnDied;
+
     public void TakeDamage(float damageAmount)
     {
         if (currentHealth == 0)
@@ -28,6 +33,11 @@ public class HealthController : MonoBehaviour
 
         if (currentHealth < 0)
             currentHealth = 0;
+
+        if (currentHealth == 0)
+        {
+            OnDied.Invoke();
+        }
 
     }
 
