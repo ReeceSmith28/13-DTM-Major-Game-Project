d794d16890428dc1bfe699508da54dec7f20d1c4
ReeceSmith28 <125227555+ReeceSmith28@users.noreply.github.com>
2023-08-10 12:32:39 +1200

    finished enemy spawning

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject slimePrefab;
+
+    [SerializeField]
+    private float minimumSpawnTime;
+
+    [SerializeField]
+    private float maximumSpawnTime;
+
+    private float timeUntilSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTimeUntilSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeUntilSpawn -= Time.deltaTime;
+
+        if (timeUntilSpawn <= 0)
+        {
+            Instantiate(slimePrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+
     }
+
+    private void SetTimeUntilSpawn()
+    {
+        timeUntilSpawn = Random.Range(minimumSpawnTime, maximumSpawnTime);
+    }
+
 }
